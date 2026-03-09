866204f8f9639134e4d61c0c62d6beabf9829029
Jonaaaata <ceballosjonatan8@gmail.com>
2026-03-08 23:45:41 -0600

    Integracion CRUD de employees

---
+++ src/employees/employees.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import { CreateEmployeeDto } from './dto/create-employee.dto';
+import { UpdateEmployeeDto } from './dto/update-employee.dto';
+
+@Injectable()
+export class EmployeesService {
+  private employees: CreateEmployeeDto[] = [{
+    id: 1,
+    name: "Jonatan",
+    lastName: "Ceballos",
+    phoneNumber: "4427891237"
+  }]
+  create(createEmployeeDto: CreateEmployeeDto) {
+    createEmployeeDto.id = this.employees.length + 1;
+    this.employees.push(createEmployeeDto);
+    return createEmployeeDto;
+  }
+
+  findAll() {
+    return this.employees;
+  }
+
+  findOne(id: number) {
+    const employee = this.employees.filter((employee)=>employee.id === id)[0];
+    return employee;
+  }
+
+  update(id: number, updateEmployeeDto: UpdateEmployeeDto) {
+  let employeeToUpdate = this.findOne(id);
+  employeeToUpdate = {
+  ... employeeToUpdate,
+  ... updateEmployeeDto
+  }
+  this.employees = this.employees.map((employee) =>{
+    if (employee.id === id){
+      employee = employee
+    }
+    return employee;
+  })
+  return employeeToUpdate;
+}
+
+  remove(id: number) {
+    this.employees = this.employees.filter((employee) => employee.id != id);
+    return this.employees;
+  }
+}
